6b7d4af65940298d50148215104e9cb29a602fd5
theerapat-pkcn <theerapat.pkcn@gmail.com>
2022-12-28 02:17:18 +0700

    [main] Fix bug credential not found

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from siambit import siambit
+from siambit.siambit import check_credential, screen_shot_scrapper
 
 def main():
   #(v) initiate credential
-  siambit.check_credential()
-  siambit.screen_shot_scrapper()
+  check_credential()
+  screen_shot_scrapper()
 
 
 if __name__ == "__main__":
